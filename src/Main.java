53f7c1143e4e30e34163ee8bc4db7f6e96a0fa17
Nguyen Phan <nqphan212@gmail.com>
2022-02-09 16:51:09 -0500

    1584. Min Cost to Connected All Points - Medium

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,46 @@
+import java.util.PriorityQueue;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+    }
+
+    //1584. Min Cost to Connect All Points - Medium
+    public int minCostConnectPoints(int[][] points) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+        int n = points.length - 1;
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = i + 1; j <= n; j++)
+            {
+                pq.offer(new int[]{i, j, distance(points[i], points[j])});
+            }
+        }
+
+        UFR ufr = new UFR(points.length);
+        int countEdges = 0;
+        int weight = 0;
+
+        while(pq.size() > 0)
+        {
+            int[] point = pq.poll();
+            if(ufr.find(point[0]) != ufr.find(point[1]))
+            {
+                ufr.union(point[0], point[1]);
+                weight += point[2];
+                n--;
+            }
+            if(n == 0)
+                break;
+        }
+
+        return weight;
+    }
+
+    public int distance(int[] x, int[] y)
+    {
+        return Math.abs(x[0] - y[0]) + Math.abs(x[1] - y[1]);
     }
 }
