655180b7510f642210766a9e6d42ebc3c8b852f9
Nguyen Phan <nqphan212@gmail.com>
2022-02-09 18:02:08 -0500

    1584. Min Cost to Connected All Points - Medium - Using Prim's Algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,49 @@
+import java.util.HashSet;
 import java.util.PriorityQueue;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+    }
+
+    //1584. Min Cost to Connect All Points - Medium - Using prim
+    public int minCostConnectPointsUsingPrimsAlgo(int[][] points) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+        int cost = 0;
+        int n = points.length;
+
+        Set<Integer> visited = new HashSet<>(); //unvisited nodes
+        visited.add(0);
+        int[] startingPoint = new int[]{points[0][0], points[0][1]}; //pick the first one, but can be any point
+
+        //find all the edges from the starting point;
+        for(int j = 1; j < points.length; j++)
+        {
+            pq.offer(new int[]{0, j, distance(startingPoint, points[j])});
+        }
+
+        while(pq.size() > 0)
+        {
+            int[] p = pq.poll();
+            if(!visited.contains(p[1]))
+            {
+                visited.add(p[1]);
+                cost += p[2];
+                for(int i = 0; i < n; i++)
+                {
+                    if(!visited.contains(i))
+                    {
+                        pq.offer(new int[]{p[2], i, distance(points[p[1]], points[i])});
+                    }
+                }
+            }
+            //terminate the loop once all nodes are connected
+            if(visited.size() == n)
+                break;
+        }
+        return cost;
     }
 
     //1584. Min Cost to Connect All Points - Medium
