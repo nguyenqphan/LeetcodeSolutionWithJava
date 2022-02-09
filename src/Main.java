881192207a629e7c49dedbd066f99d2209ed3bc8
Nguyen Phan <nqphan212@gmail.com>
2022-02-09 18:37:22 -0500

    1168. Optimize Water Distribution in a Village - Hard - Using Kruska and UF

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,35 @@ public class Main {
 	// write your code here
     }
 
+    //1168. Optimize Water Distribution in a Village - Using Kruskal's Algo and Union Find
+    public int minCostToSupplyWater(int n, int[] wells, int[][] pipes) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+        int cost = 0;
+
+        for(int i = 1; i <= n; i++)
+        {
+            pq.offer(new int[]{0, i, wells[i - 1]});
+        }
+
+        for(int[] pipe : pipes)
+        {
+            pq.offer(new int[]{pipe[0], pipe[1], pipe[2]});
+        }
+
+        UFR urf = new UFR(n + 1);
+        while(pq.size() > 0)
+        {
+            int[] p = pq.poll();
+            if(urf.find(p[0]) != urf.find(p[1]))
+            {
+                cost += p[2];
+                urf.union(p[0], p[1]);
+                n--;
+            }
+        }
+        return cost;
+    }
+
     //1584. Min Cost to Connect All Points - Medium - Using prim
     public int minCostConnectPointsUsingPrimsAlgo(int[][] points) {
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
