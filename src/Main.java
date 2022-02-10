3409c55b0b24b297b384f744f12bcd592c8857fc
Nguyen Phan <nqphan212@gmail.com>
2022-02-10 15:51:36 -0500

    198. House Robber - Medium - Using DP

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,24 @@ public class Main {
 	// write your code here
     }
 
+    //198. House Robber - Medium - Using Dp
+    public int rob(int[] nums) {
+        if(nums.length == 1)
+            return nums[0];
+
+        int[] money = new int[nums.length];
+
+        money[0] = nums[0];
+        money[1] = Math.max(nums[0], nums[1]);
+
+        for(int i = 2; i < nums.length; i++)
+        {
+            money[i] = Math.max(nums[i] + money[i - 2], money[i - 1]);
+        }
+
+        return money[nums.length - 1];
+
+    }
     //505. The Maze II - Medium - Using Dijkstra and PriorityQueue
     public int shortestDistance(int[][] maze, int[] start, int[] destination) {
         int[][] distance = new int[maze.length][maze[0].length];
