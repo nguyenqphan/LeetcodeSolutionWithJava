91fb7bb2fb7bd62b7e4e42e407cc89775fd196a3
Nguyen Phan <nqphan212@gmail.com>
2022-02-11 17:06:02 -0500

    1143. Longest Common Subsequence - Medium - Bottom Up DP

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,27 @@ public class Main {
     }
 
     //1143. Longest Common Subsequence - Medium - Top Down DP
+    public int longestCommonSubsequence_BottomUpDP(String text1, String text2) {
+        int[][] memo = new int[text1.length() + 1][text2.length() + 1];
+
+        for(int i = text1.length() - 1; i >= 0; i--)
+        {
+            for(int j = text2.length() - 1; j >= 0; j--)
+            {
+                if(text1.charAt(i) == text2.charAt(j))
+                {
+                    memo[i][j] = 1 + memo[i + 1][j + 1];
+                }
+                else
+                {
+                    memo[i][j] = Math.max(memo[i][j + 1], memo[i + 1][j]);
+                }
+            }
+        }
+
+        return memo[0][0];
+    }
+    //1143. Longest Common Subsequence - Medium - Top Down DP
     public int longestCommonSubsequence(String text1, String text2) {
         return longestCommonSubsequenceHelper(text1, text2, 0, 0, new Integer[text1.length()][text2.length()]);
     }
