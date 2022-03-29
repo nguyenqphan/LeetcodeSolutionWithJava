d6ca3c667e35471fd253819d48cdb0979576fe3d
Nguyen Phan <nqphan212@gmail.com>
2022-03-29 13:14:34 -0400

    7. Reverse Integer- Medium

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,22 @@ public class Main {
     public static void main(String[] args) {
     }
 
-    //6. Zizzag conversion - Medium 
+    //7. Reverse Integer - Medium
+    public static int reverse(int x) {
+        int rev = 0;
+        while(x!= 0)
+        {
+            int rem = x % 10;
+            x /= 10;
+            if(rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE/10 && rem > 7) ||
+                    rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE/10 && rem < -8))
+                return 0;
+
+            rev = rev * 10 + rem;
+        }
+        return rev;
+    }
+    //6. Zizzag conversion - Medium
     public static String convert(String s, int numRows) {
         if(numRows == 1)
             return s;
