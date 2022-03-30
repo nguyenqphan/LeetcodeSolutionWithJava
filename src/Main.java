2c85c2ae118dfa51463d584a1f4d9effcce5463a
Nguyen Phan <nqphan212@gmail.com>
2022-03-30 00:59:05 -0400

    34. Find the first and last position of element in sorted array - Medium

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,51 @@ import static java.util.List.*;
 
 public class Main {
     public static void main(String[] args) {}
+    //32. Find the First and Last Position of Element in a sorted Array - Medium
+    public static int[] searchRange(int[] nums, int target) {
+        if(nums.length == 0)
+            return new int[]{-1, -1};
+
+        int leftMost = searchRangeHelper(nums, target, true);
+        if(leftMost == -1)
+            return new int[]{-1, -1};
+
+        int rightMost = searchRangeHelper(nums, target, false);
+        return new int[]{leftMost, rightMost};
+    }
+
+    public static int searchRangeHelper(int[] nums, int target, boolean isFirst)
+    {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left <= right)
+        {
+            int mid = left - (left - right)/2;
+            if(nums[mid] == target)
+            {
+                if(isFirst)
+                {
+                    if((mid == left) || (nums[mid] != nums[mid - 1]))
+                        return mid;
+                    right = mid - 1;
+                }
+                else
+                {
+                    if((mid == right) || (nums[mid] != nums[mid + 1]))
+                        return mid;
+                    left = mid + 1;
+                }
+            }
+            else if(nums[mid] > target)
+            {
+                right = mid -1 ;
+            }
+            else
+                left = mid + 1;
+        }
+        return -1;
+    }
     //16. 3sum closest - medium
     public static int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
