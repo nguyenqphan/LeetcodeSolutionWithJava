e9d7a2a5613a78a788e8ababf2ed82a8b9ecc074
Nguyen Phan <nqphan212@gmail.com>
2023-03-06 09:48:34 -0500

    190. Reserve bits - easy - mask and bit shift

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,20 @@ public class Main {
 
     }
 
+    // 190. RESERVE BITS - EASY - MASK AND BIT SHIFT
+    public static int reverseBits(int num) {
+        num = ((num & 0xffff0000) >>> 16) | ((num & 0x0000ffff) << 16);
+        num = ((num & 0xff00ff00) >>> 8) | ((num & 0x00ff00ff) << 8);
+        num = ((num & 0xf0f0f0f0) >>> 4) | ((num & 0x0f0f0f0f) << 4);
+        num = ((num & 0xcccccccc) >>> 2) | ((num & 0x33333333) << 2);
+        num = ((num & 0xaaaaaaaa) >>> 1) | ((num & 0x55555555) << 1);
+        // n = ((n & 0b11111111111111110000000000000000) >>> 16) | ((n & 0b00000000000000001111111111111111) << 16);
+        // n = ((n & 0b11111111000000001111111100000000) >>> 8)  | ((n & 0b00000000111111110000000011111111) << 8);
+        // n = ((n & 0b11110000111100001111000011110000) >>> 4)  | ((n & 0b00001111000011110000111100001111) << 4);
+        // n = ((n & 0b11001100110011001100110011001100) >>> 2)  | ((n & 0b00110011001100110011001100110011) << 2);
+        // n = ((n & 0b10101010101010101010101010101010) >>> 1)  | ((n & 0b01010101010101010101010101010101) << 1);
+        return num;
+    }
     //975. ODD EVEN JUMP - Hard
     public static int oddEvenJumps(int[] arr) {
         int res = 1; //alway reach the end staring at last index
